cbd05674fd05b9069767a548b76a0f94d31ad92c
sly-GD <2572137430@qq.com>
2026-06-04 13:52:21 +0800

    commit 2273

---
+++ leetcode_test/2273. 移除字母异位词后的结果数组.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=2273 lang=cpp
+ *
+ * [2273] 移除字母异位词后的结果数组
+ */
+#include <bits/stdc++.h> 
+using namespace std;
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) {
+        int n=words.size();
+        int k=0;
+        string base="";
+        for(auto& word:words){
+            string s=word;
+            std::ranges::sort(s);
+            if(s!=base){
+                base=move(s); //浅拷贝
+                words[k++]=word;
+            }
+        }
+        words.resize(k);
+        return words;  
+
+    }
+};
+ // @lc code=end
+
