bf9afc9078afe9d489f03ed0770df8cb214c17e6
itachi <102279625+sly-GD@users.noreply.github.com>
2026-07-14 16:55:52 +0800

    Implement getCommon function to find common value

---
+++ leetcode_test/2540. 最小公共值.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size(),m=nums2.size();
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(nums1[i]==nums2[j]){
+                return nums1[i];
+            }
+            if(nums1[i]>nums2[j]){
+                j++;
+            }else{
+                i++;
+            }
+        }
+        return -1;
+    }
+};
